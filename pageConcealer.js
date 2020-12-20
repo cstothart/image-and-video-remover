e702b52b84e995809b6243abca6a816295c25d47
cstothart <cary.stothart@gmail.com>
2020-12-20 14:56:01 -0600

    Added iframe support to removeMedia function.

--- pageConcealer.js
+++ pageConcealer.js
@@ -1,7 +1,5 @@
 const concealer = document.createElement('div');
 concealer.id = 'concealer';
 const concealerText = document.createTextNode('Hiding images and videos...');
-concealer.style.width = `${window.innerWidth}px`;
-concealer.style.height = `${window.innerHeight}px`;
 concealer.appendChild(concealerText);
 document.documentElement.appendChild(concealer);
--- removeMedia.js
+++ removeMedia.js
@@ -1,17 +1,41 @@
 const removeMedia = () => {
-  const images = document.querySelectorAll('img, image, svg, video');
-  if(images.length) {
-    images.forEach((image, index) => {
-      image.style.visibility = 'hidden';
-      if(index === images.length - 1) {
-        document.querySelector('#concealer').style.display = 'none';
-      }
-    })
-  }
+    const media = document.querySelectorAll('img, image, svg, video, iframe');
+    if(media.length) {
+      media.forEach((mediaElement, index) => {
+        const elementTag = mediaElement.tagName.toLowerCase();
+        if(elementTag === 'iframe') {
+          try {
+            const iframeDoc = mediaElement.contentDocument || 
+              mediaElement.contentWindow.document;
+            const iframeMedia = 
+              iframeDoc.querySelectorAll('img, image, svg, video');
+            iframeMedia.forEach(iframeMediaElement => {
+              iframeMediaElement.style.visibility = 'hidden';
+            });
+          } catch(err) {
+            console.log('Unable to remove images and videos from iframe: ', 
+              err);
+          }
+        } else {
+          mediaElement.style.visibility = 'hidden';
+        }
+        if(index === media.length - 1) {
+          document.querySelector('#concealer').style.display = 'none';
+        }
+      });
+    }
 }
 
-observerCallback = mutations => {
+const removeBackgroundImages = () => {
+  const elements = document.querySelectorAll('*');
+  elements.forEach(element => {
+    element.style.backgroundImage = '';
+  })
+}
+
+const observerCallback = mutations => {
   removeMedia();
+  removeBackgroundImages();
 }
 
 const observerTarget = document.querySelector('body');
